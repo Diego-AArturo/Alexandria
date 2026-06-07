360a017cf77b3a10d28ae81a9edbe97ca02823a9
Diego-AArturo <diegullo201@gmail.com>
2026-06-07 17:04:18 -0500

    test main

--- movil_app/alexandria_movil/lib/main.dart
+++ movil_app/alexandria_movil/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:alexandria_movil/data/notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:alexandria_movil/l10n/app_localizations.dart';
 
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotificationService().init();
